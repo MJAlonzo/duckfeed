9f47c276c91b43f1cfb03f53d6560bedd94ff77b
mjalonzo <marc.j.alonzo@gmail.com>
2020-05-30 13:21:43 -0700

    Add cancel and submit button. Console log when submit is clicked and reset whern cancel

--- src/App.js
+++ src/App.js
@@ -2,8 +2,11 @@ import React, { useState } from "react";
 
 import { makeStyles } from "@material-ui/core/styles";
 import Grid from "@material-ui/core/Grid";
-import Paper from "@material-ui/core/Paper";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import CardActions from "@material-ui/core/CardActions";
 import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
 
 import "./App.css";
 
@@ -15,12 +18,12 @@ const useStyles = makeStyles((theme) => ({
   grid: {
     height: "100%",
   },
-  paper: {
-    padding: theme.spacing(4),
-    textAlign: "center",
-    color: theme.palette.text.secondary,
-  },
 }));
+
+const numberFieldProps = {
+  type: "number",
+  min: 0,
+};
 
 function App() {
   const [ducks, setDucks] = useState(0);
@@ -31,6 +34,22 @@ function App() {
 
   const classes = useStyles();
 
+  function handleSubmit() {
+    console.log(ducks);
+    console.log(time);
+    console.log(location);
+    console.log(foodType);
+    console.log(foodAmount);
+  }
+
+  function reset() {
+    setDucks(0);
+    setTime("");
+    setLocation("");
+    setFoodType("");
+    setFoodType("");
+  }
+
   return (
     <div className={classes.root}>
       <Grid
@@ -40,59 +59,78 @@ function App() {
         alignItems="center"
       >
         <Grid item xs={6}>
-          <Paper className={classes.paper}>
-            <TextField
-              id="ducks"
-              fullWidth
-              label="How Many Ducks"
-              value={ducks}
-              inputProps={{ type: "number" }}
-              margin="normal"
-              onChange={(e) => {
-                setDucks(e.target.value);
-              }}
-            />
-            <TextField
-              id="time"
-              fullWidth
-              label="Time"
-              value={time}
-              margin="normal"
-              onChange={(e) => {
-                setTime(e.target.value);
-              }}
-            />
-            <TextField
-              id="location"
-              fullWidth
-              label="Location"
-              value={location}
-              margin="normal"
-              onChange={(e) => {
-                setLocation(e.target.value);
-              }}
-            />
-            <TextField
-              id="foodType"
-              fullWidth
-              label="Food Type"
-              value={foodType}
-              margin="normal"
-              onChange={(e) => {
-                setFoodType(e.target.value);
-              }}
-            />
-            <TextField
-              id="foodAmount"
-              fullWidth
-              label="Food Amount"
-              value={foodAmount}
-              margin="normal"
-              onChange={(e) => {
-                setFoodAmount(e.target.value);
-              }}
-            />
-          </Paper>
+          <Card>
+            <CardContent>
+              <TextField
+                id="ducks"
+                fullWidth
+                label="How Many Ducks"
+                value={ducks}
+                inputProps={numberFieldProps}
+                margin="normal"
+                onChange={(e) => {
+                  setDucks(e.target.value);
+                }}
+              />
+              <TextField
+                id="time"
+                fullWidth
+                label="Time"
+                value={time}
+                margin="normal"
+                onChange={(e) => {
+                  setTime(e.target.value);
+                }}
+              />
+              <TextField
+                id="location"
+                fullWidth
+                label="Location"
+                value={location}
+                margin="normal"
+                onChange={(e) => {
+                  setLocation(e.target.value);
+                }}
+              />
+              <TextField
+                id="foodType"
+                fullWidth
+                label="Food Type"
+                value={foodType}
+                margin="normal"
+                onChange={(e) => {
+                  setFoodType(e.target.value);
+                }}
+              />
+              <TextField
+                id="foodAmount"
+                fullWidth
+                label="Food Amount"
+                value={foodAmount}
+                inputProps={numberFieldProps}
+                margin="normal"
+                onChange={(e) => {
+                  setFoodAmount(e.target.value);
+                }}
+              />
+            </CardContent>
+            <CardActions>
+              <Button
+                onClick={() => {
+                  reset();
+                }}
+              >
+                Cancel
+              </Button>
+              <Button
+                variant="contained"
+                color="primary"
+                onClick={() => handleSubmit()}
+              >
+                Submit
+              </Button>
+            </CardActions>
+          </Card>
         </Grid>
       </Grid>
     </div>
