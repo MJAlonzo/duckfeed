772b965e1bf2f0e307da6159a8efbc6d5da4db07
mjalonzo <marc.j.alonzo@gmail.com>
2020-06-02 23:35:24 -0700

    Add Form tests and improve Dashboard test

--- src/tests/modules/dashboard/Dashboard.test.js
+++ src/tests/modules/dashboard/Dashboard.test.js
@@ -7,18 +7,35 @@ import Dashboard from "../../../modules/dashboard/Dashboard";
 
 window.MutationObserver = MutationObserver;
 
-const db = jest.fn(() => {
-  const get = async () => {
-    const data = {
-      docs: [await Promise.resolve(import("./serverFeedFixture"))],
-    };
-
-    return data;
-  };
+jest.mock("firebase", () => {
+  const get = async () =>
+    await Promise.resolve({
+      docs: [
+        {
+          data: () => ({
+            id: 1591081534749,
+            ducks: 6,
+            date: "2020-06-02",
+            time: "13:05",
+            location: "1691 Pear St, Victoria, BC V8P 2A5, Canada",
+            food: {
+              type: "birdFeed",
+              amount: 10,
+            },
+          }),
+        },
+      ],
+    });
 
   return {
-    collection: jest.fn(() => ({
-      get,
+    initializeApp: jest.fn(() => ({
+      firestore: jest.fn(
+        jest.fn(() => ({
+          collection: jest.fn(() => ({
+            get,
+          })),
+        }))
+      ),
     })),
   };
 });
@@ -32,5 +49,7 @@ describe("<Dashboard/>", () => {
     expect(
       getByText("1691 Pear St, Victoria, BC V8P 2A5, Canada")
     ).toBeInTheDocument();
+
+    expect(getByText("2020-06-02 - 13:05")).toBeInTheDocument();
   });
 });
